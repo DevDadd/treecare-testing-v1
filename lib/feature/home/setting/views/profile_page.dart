d5cd02cfef1000a31f850ed0d0606443159af66c
DevDad123 <anhnq.b23dt013@stu.ptit.edu.vn>
2025-09-08 23:10:37 +0700

    Done Profile Page

--- lib/feature/home/setting/views/profile_page.dart
+++ lib/feature/home/setting/views/profile_page.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:testtree/cubit/tree_cubit.dart';
 import 'package:testtree/cubit/tree_state.dart';
 import 'package:testtree/cubit/user_cubit.dart';
 import 'package:testtree/cubit/user_state.dart';
+import 'package:testtree/router/go_router.dart';
 import 'package:testtree/widget/setting_widget.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -20,68 +22,201 @@ class ProfilePage extends StatelessWidget {
     final size = MediaQuery.of(context).size;
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 241, 241, 241),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-      ),
+      appBar: AppBar(backgroundColor: Colors.white),
       body: BlocBuilder<TreeCubit, TreeState>(
         builder: (context, treestate) {
           return BlocBuilder<UserCubit, UserState>(
-              builder: (context, state) {
-                if(state.users == null){
-                  return CircularProgressIndicator();
-                }
-                final imageUrl = state.users!.imageUrl;
-                debugPrint("Image URL: ${state.users?.imageUrl}");
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Center(child: CircleAvatar(radius: 70,backgroundImage: NetworkImage(imageUrl))),
-                      SizedBox(height: 15,),
-                      Text(state.users!.userName, style: GoogleFonts.cairo(fontWeight: FontWeight.w700, fontSize: 20,color: Colors.green),),
-                      SizedBox(height: 15,),
-                      Container(
-                        height: size.height * 0.04,
-                        width: size.width * 0.5,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: const Color.fromARGB(255, 255, 255, 255),
+            builder: (context, state) {
+              if (state.users == null) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              final imageUrl = state.users!.imageUrl;
+              debugPrint("Image URL: ${state.users?.imageUrl}");
+              return Center(
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                          radius: 70,
+                          backgroundImage: NetworkImage(imageUrl),
                         ),
-                        child: Row(
-                          children: [
-                            Center(child: Padding(
+                        const SizedBox(height: 15),
+                        Text(
+                          state.users!.userName,
+                          style: GoogleFonts.cairo(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20,
+                            color: Colors.green,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 15),
+                        Container(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 12,
+                            vertical: 8,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white,
+                          ),
+                          child: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Number leaves detected: ",
+                                style: GoogleFonts.cairo(color: Colors.green),
+                              ),
+                              const SizedBox(width: 2),
+                              Text(
+                                treestate.treeHistory.length.toString(),
+                                style: GoogleFonts.cairo(
+                                  color: Colors.green,
+                                  fontWeight: FontWeight.w700,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        IntrinsicWidth(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Padding(
                               padding: const EdgeInsets.all(8.0),
-                              child: Text("Number leaves detected: ",style: GoogleFonts.cairo(color: Colors.green),),
-                            )),
-                            SizedBox(width: 2,),
-                            Center(child: Text(treestate.treeHistory.length.toString(),style: GoogleFonts.cairo(color: Colors.green),)),
-                          ],
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  SettingWidget(
+                                    icons: icons[0],
+                                    title: "Change Theme",
+                                  ),
+                                  const Divider(color: Colors.black),
+                                  SettingWidget(
+                                    icons: icons[1],
+                                    title: "Change Language",
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 15,),
-                      Container(
-                        height: size.height * 0.09,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(10),
+                        const SizedBox(height: 15),
+                        IntrinsicWidth(
+                          child: GestureDetector(
+                            onTap: () {
+                              showLogoutDialog(context, () {
+                                context.push(AppRouteConstant.initial);
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 16,
+                                  vertical: 10,
+                                ),
+                                child: Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      "Sign Out",
+                                      style: GoogleFonts.cairo(
+                                        color: Colors.green,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                    const SizedBox(width: 6),
+                                    const Icon(
+                                      Icons.exit_to_app,
+                                      color: Colors.green,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: ListView.separated(itemCount: 2, itemBuilder: (context, index) {
-                            return SettingWidget(icons: icons[index], title: "Change Theme",);
-                          }, separatorBuilder: (context, index) {
-                            return Divider(color: Colors.black,);
-                          },),
-                        ),
-                      )
-                    ],
+                      ],
+                    ),
                   ),
-                );
-              },
-            );
+                ),
+              );
+            },
+          );
         },
       ),
     );
   }
+}
+
+Future<void> showLogoutDialog(
+  BuildContext context,
+  VoidCallback onConfirm,
+) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        title: Center(
+          child: Text(
+            "Logging out...",
+            style: GoogleFonts.cairo(
+              color: Colors.green,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+        content: Text(
+          "Are you sure you want to log out?",
+          style: GoogleFonts.cairo(color: Colors.green),
+          textAlign: TextAlign.center,
+        ),
+        actionsAlignment: MainAxisAlignment.center,
+        actions: <Widget>[
+          TextButton(
+            child: Text(
+              "Cancel",
+              style: GoogleFonts.cairo(color: Colors.green),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+          TextButton(
+            child: Text(
+              "Log out",
+              style: GoogleFonts.cairo(
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+              onConfirm();
+            },
+          ),
+        ],
+      );
+    },
+  );
 }
