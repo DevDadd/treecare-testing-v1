ed6b904270db80db71caa9f4a3c9de36c511f188
DevDad123 <anhnq.b23dt013@stu.ptit.edu.vn>
2025-09-14 23:57:04 +0700

    Initialize Result Detail Page

---
+++ lib/feature/home/setting/views/result_detail_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ResultDetailPage extends StatelessWidget {
+  ResultDetailPage({super.key, required this.id});
+  final int id;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
